460b5b1fd4b572d539450bd0e182cf3eab91e545
Cyril Solé <cyril.solesala@thalesgroup.com>
2022-03-25 23:07:04 +0100

    Add Source and Destination respectively for Ingress and Egress

--- shell/listSecurityGroupRules.sh
+++ shell/listSecurityGroupRules.sh
@@ -11,18 +11,27 @@ vpcId=`aws $AWS_OPTS ec2 describe-vpcs --region $region --filters Name=tag:Name,
 echo "List all Security Group Rules for VPC $vpcName($vpcId) in $region"
 
 #Ingress
-aws $AWS_OPTS ec2 describe-security-groups --region $region --filters Name=vpc-id,Values=$vpcId --query SecurityGroups[].IpPermissions > $TMPFILE
+aws $AWS_OPTS ec2 describe-security-groups --region $region --filters Name=vpc-id,Values=$vpcId \
+                  --query "SecurityGroups[].{IpPermissions:IpPermissions[], Destination: GroupId}" > $TMPFILE
 
-echo "From Port\\tTo Port\\tSource\\tDescription" > $INGRESS
-jq -cr '.[][] as $parent | $parent.IpRanges[]         | [($parent.FromPort, $parent.ToPort, .CidrIp,       .Description)] | @tsv' $TMPFILE >> $INGRESS
-jq -cr '.[][] as $parent | $parent.UserIdGroupPairs[] | [($parent.FromPort, $parent.ToPort, .GroupId,      .Description)] | @tsv' $TMPFILE >> $INGRESS
-jq -cr '.[][] as $parent | $parent.PrefixListIds[]    | [($parent.FromPort, $parent.ToPort, .PrefixListId, .Description)] | @tsv' $TMPFILE >> $INGRESS
+echo "From Port\\tTo Port\\tSource\\tDestination\\tDescription" > $INGRESS
+jq -cr '.[] as $parent | $parent.IpPermissions[] as $perm | $perm.IpRanges[]         
+        | [$perm.FromPort, $perm.ToPort,  .CidrIp,       $parent.Destination, .Description] | @tsv'  $TMPFILE >> $INGRESS
+jq -cr '.[] as $parent | $parent.IpPermissions[] as $perm | $perm.UserIdGroupPairs[] 
+        | [$perm.FromPort, $perm.ToPort,  .GroupId,      $parent.Destination, .Description] | @tsv'  $TMPFILE >> $INGRESS
+jq -cr '.[] as $parent | $parent.IpPermissions[] as $perm | $perm.PrefixListIds[]    
+        | [$perm.FromPort, $perm.ToPort,  .PrefixListId, $parent.Destination, .Description] | @tsv'  $TMPFILE >> $INGRESS
+        
 
 #Egress
-aws $AWS_OPTS ec2 describe-security-groups --region $region --filters Name=vpc-id,Values=$vpcId --query SecurityGroups[].IpPermissionsEgress > $TMPFILE
-echo "From Port\\tTo Port\\tDestination\\tDescription" > $EGRESS
-jq -cr '.[][] as $parent | $parent.IpRanges[]         | [($parent.FromPort, $parent.ToPort, .CidrIp,       .Description)] | @tsv' $TMPFILE >> $EGRESS
-jq -cr '.[][] as $parent | $parent.UserIdGroupPairs[] | [($parent.FromPort, $parent.ToPort, .GroupId,      .Description)] | @tsv' $TMPFILE >> $EGRESS
-jq -cr '.[][] as $parent | $parent.PrefixListIds[]    | [($parent.FromPort, $parent.ToPort, .PrefixListId, .Description)] | @tsv' $TMPFILE >> $EGRESS
+aws $AWS_OPTS ec2 describe-security-groups --region $region --filters Name=vpc-id,Values=$vpcId \
+                  --query "SecurityGroups[].{IpPermissionsEgress:IpPermissionsEgress[], Source: GroupId}" > $TMPFILE
+echo "From Port\\tTo Port\\tSource\\tDestination\\tDescription" > $EGRESS
+jq -cr '.[] as $parent | $parent.IpPermissionsEgress[] as $perm | $perm.IpRanges[]         
+        | [$perm.FromPort, $perm.ToPort, $parent.Source, .CidrIp,       .Description] | @tsv'  $TMPFILE >> $EGRESS
+jq -cr '.[] as $parent | $parent.IpPermissionsEgress[] as $perm | $perm.UserIdGroupPairs[] 
+        | [$perm.FromPort, $perm.ToPort, $parent.Source, .GroupId,      .Description] | @tsv'  $TMPFILE >> $EGRESS
+jq -cr '.[] as $parent | $parent.IpPermissionsEgress[] as $perm | $perm.PrefixListIds[]    
+        | [$perm.FromPort, $perm.ToPort, $parent.Source, .PrefixListId, .Description] | @tsv'  $TMPFILE >> $EGRESS
 
 rm -rf $TMPFILE
