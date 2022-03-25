222eb810670c88a47a537a9ee18ad573da832467
Cyril Solé <cyril.solesala@thalesgroup.com>
2022-03-25 23:07:28 +0100

    MAke the script working when PWD is not in the PATH

--- shell/switchRole.sh
+++ shell/switchRole.sh
@@ -16,6 +16,6 @@ function aws_who {
 }
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
+chmod +x ./kubectl
 
 export PATH=.:${PATH}
